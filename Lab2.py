06a13042d728fdfe5c005385d3aad2a8aff8706a
Balaji <mbalaji06.23@ichat.sp.edu.sg>
2024-04-25 16:08:29 +0800

    Updated code

--- Lab2.py
+++ Lab2.py
@@ -19,8 +19,18 @@ def calc_min_max_temperature(num):
         if max < i:
             max = i
     return [min,max] 
+def calc_median_temperature(num):
+    num.sort()
+    if len(num)%2 == 0:
+        median = (num[len(num)//2]+num[len(num)//2-1])/2
+    else:
+        median = (num[len(num)//2])  
+    return median      
+
+
 display_main_menu()
 num = get_user_input()
 print("Average temperature is " + str(calc_average_temperature(num)))
 print("Minimum temperature is " + str(calc_min_max_temperature(num)[0]))       
 print("Maximum temperature is " + str(calc_min_max_temperature(num)[1]))  
+print("Median temperature is " + str(calc_median_temperature(num)))
